cb9729d961dd8b80c427fc9d27bf5c8d5987b4c6
Junit38 <Junit38@gmail.com>
2020-11-24 17:25:21 +0100

    norm

--- srcs/dir.c
+++ srcs/dir.c
@@ -6,7 +6,7 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/11/24 17:23:15 by mery             ###   ########.fr       */
+/*   Updated: 2020/11/24 17:25:19 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int		is_dir(char *path)
 {
 	struct stat sb;
 
-	if (stat(path, &sb) != -1) {
+	if (stat(path, &sb) != -1)
+	{
 		if (S_ISDIR(sb.st_mode))
 			return (1);
 	}
@@ -27,9 +28,8 @@ int		is_file(char *path)
 {
 	struct stat sb;
 
-	if (stat(path, &sb) != -1) {
+	if (stat(path, &sb) != -1)
 		return (1);
-	}
 	return (0);
 }
 
@@ -37,8 +37,7 @@ int		get_time(char *path)
 {
 	struct stat sb;
 
-	if (stat(path, &sb) != -1) {
-	   return (sb.st_ctime);
-	}
+	if (stat(path, &sb) != -1)
+		return (sb.st_ctime);
 	return (0);
 }
