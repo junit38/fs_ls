cec47c84f4c0239e2c0290e43a94a9758e3b0e32
Junit38 <Junit38@gmail.com>
2020-11-24 17:23:20 +0100

    norm

--- srcs/dir.c
+++ srcs/dir.c
@@ -6,39 +6,39 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/11/24 17:01:38 by mery             ###   ########.fr       */
+/*   Updated: 2020/11/24 17:23:15 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int 		is_dir(char *path)
+int		is_dir(char *path)
 {
 	struct stat sb;
 
 	if (stat(path, &sb) != -1) {
-        if (S_ISDIR(sb.st_mode))
-        	return (1);
-    }
-    return (0);
+		if (S_ISDIR(sb.st_mode))
+			return (1);
+	}
+	return (0);
 }
 
-int 		is_file(char *path)
+int		is_file(char *path)
 {
 	struct stat sb;
 
 	if (stat(path, &sb) != -1) {
-        return (1);
-    }
-    return (0);
+		return (1);
+	}
+	return (0);
 }
 
-int         get_time(char *path)
+int		get_time(char *path)
 {
-    struct stat sb;
+	struct stat sb;
 
-    if (stat(path, &sb) != -1) {
-       return (sb.st_ctime);
-    }
-    return (0);
+	if (stat(path, &sb) != -1) {
+	   return (sb.st_ctime);
+	}
+	return (0);
 }
