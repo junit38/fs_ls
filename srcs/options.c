a4953c568dd9a76222cfe6eeb5d68d360c58bae9
Junit38 <Junit38@gmail.com>
2020-11-25 12:32:49 +0100

    norm

--- srcs/options.c
+++ srcs/options.c
@@ -6,7 +6,7 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/11/25 11:57:48 by mery             ###   ########.fr       */
+/*   Updated: 2020/11/25 12:32:30 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void	init_options_2(t_data *data, char *argv)
 {
-	if (ft_strcmp(argv, "-l") == 0)
+	if (ft_strchr(argv, 'l'))
 		data->l = 1;
-	else if (ft_strcmp(argv, "-R") == 0)
+	if (ft_strchr(argv, 'R'))
 		data->rr = 1;
-	else if (ft_strcmp(argv, "-a") == 0)
+	if (ft_strchr(argv, 'a'))
 		data->a = 1;
-	else if (ft_strcmp(argv, "-r") == 0)
+	if (ft_strchr(argv, 'r'))
 		data->r = 1;
-	else if (ft_strcmp(argv, "-t") == 0)
+	if (ft_strchr(argv, 't'))
 		data->t = 1;
-	else if (ft_strcmp(argv, "-G") == 0)
+	if (ft_strchr(argv, 'G'))
 		data->gg = 1;
-	else if (ft_strcmp(argv, "-1") == 0)
+	if (ft_strchr(argv, '1'))
 		data->one = 1;
 }
 
